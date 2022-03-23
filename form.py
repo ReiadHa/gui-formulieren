3662b53a4e74ea22cb8208487dff0908c1e607fe
Riad Hak <99054420@mydavinci.nl>
2022-03-23 12:25:30 +0100

    update

--- form.py
+++ form.py
@@ -1,9 +1,13 @@
 import tkinter as tk
 
 window = tk.Tk()
+window.geometry('300x600')
 
+label1 = tk.Label(text='voornaam: ')
+label1.place(relx=0.11,rely=0.02,anchor='center')
+label2 = tk.Label(text='achternaam: ')
 
-
+radioGelsacht = tk.Radiobutton()
 
 
 
